8ef14415f0d2d45d378589cae37b1febef681012
= <litvinonada@solit.by>
2023-02-01 15:58:48 +0300

    feat: add template for user

--- test/endpoints.ts
+++ test/endpoints.ts
@@ -1,40 +1,40 @@
 export const usersRoutes = {
   getAll: '/user',
-  getById: (userId) => `/user/${userId}`,
+  getById: userId => `/user/${userId}`,
   create: '/user',
-  update: (userId) => `/user/${userId}`,
-  delete: (userId) => `/user/${userId}`,
+  update: userId => `/user/${userId}`,
+  delete: userId => `/user/${userId}`,
 };
 
 export const artistsRoutes = {
   getAll: '/artist',
-  getById: (artistId) => `/artist/${artistId}`,
+  getById: artistId => `/artist/${artistId}`,
   create: '/artist',
-  update: (artistId) => `/artist/${artistId}`,
-  delete: (artistId) => `/artist/${artistId}`,
+  update: artistId => `/artist/${artistId}`,
+  delete: artistId => `/artist/${artistId}`,
 };
 
 export const albumsRoutes = {
   getAll: '/album',
-  getById: (albumId) => `/album/${albumId}`,
+  getById: albumId => `/album/${albumId}`,
   create: '/album',
-  update: (albumId) => `/album/${albumId}`,
-  delete: (albumId) => `/album/${albumId}`,
+  update: albumId => `/album/${albumId}`,
+  delete: albumId => `/album/${albumId}`,
 };
 
 export const tracksRoutes = {
   getAll: '/track',
-  getById: (trackId) => `/track/${trackId}`,
+  getById: trackId => `/track/${trackId}`,
   create: '/track',
-  update: (trackId) => `/track/${trackId}`,
-  delete: (trackId) => `/track/${trackId}`,
+  update: trackId => `/track/${trackId}`,
+  delete: trackId => `/track/${trackId}`,
 };
 
 export const favoritesRoutes = {
   getAll: '/favs',
-  artists: (artistId) => `/favs/artist/${artistId}`,
-  albums: (albumId) => `/favs/album/${albumId}`,
-  tracks: (trackId) => `/favs/track/${trackId}`,
+  artists: artistId => `/favs/artist/${artistId}`,
+  albums: albumId => `/favs/album/${albumId}`,
+  tracks: trackId => `/favs/track/${trackId}`,
 };
 
 export const authRoutes = {

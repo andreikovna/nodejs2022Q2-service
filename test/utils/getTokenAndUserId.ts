834cd4dc484b28b45ee570532a09c26f42142e81
= <litvinonada@solit.by>
2023-02-03 16:45:16 +0300

    revert tests lint

--- test/utils/getTokenAndUserId.ts
+++ test/utils/getTokenAndUserId.ts
@@ -5,14 +5,20 @@ const createUserDto = {
   password: 'Tu6!@#%&',
 };
 
-const getTokenAndUserId = async request => {
+const getTokenAndUserId = async (request) => {
   // create user
-  const response = await request.post(authRoutes.signup).set('Accept', 'application/json').send(createUserDto);
+  const response = await request
+    .post(authRoutes.signup)
+    .set('Accept', 'application/json')
+    .send(createUserDto);
 
   const mockUserId = response.body.id;
 
   // get token
-  const response2 = await request.post(authRoutes.login).set('Accept', 'application/json').send(createUserDto);
+  const response2 = await request
+    .post(authRoutes.login)
+    .set('Accept', 'application/json')
+    .send(createUserDto);
 
   const token = `Bearer ${response2.body.accessToken}`;
 

d0d91ddeb7fcd90da78920de36b52089f45fcd0b
= <litvinonada@solit.by>
2023-02-13 16:53:57 +0300

    feat: user entity and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -8,7 +10,29 @@ import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favs/favorites.module';
 
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavoritesModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DB'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        logging: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,3 +1,6 @@
+import { Exclude, Transform } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
+
 export interface IUser {
   id: string;
   login: string;
@@ -7,4 +10,23 @@ export interface IUser {
   updatedAt: number;
 }
 
-// export class User {}
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid') id: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  login: string;
+
+  @VersionColumn()
+  version: number;
+  @Transform(({ value }) => new Date(value).getTime())
+  createdDate: number;
+
+  @UpdateDateColumn()
+  @Transform(({ value }) => new Date(value).getTime())
+  updatedDate: number;
+}
